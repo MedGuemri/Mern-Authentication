f9b04402b0228335fa7049be050482c9803abf38
Mohamed Guemri <mohamedamiineguemri@gmail.com>
2024-08-20 00:57:18 +0200

    build the project

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,13 @@ import cors from "cors"
 
 import authRoutes from "./routes/authRoutes.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 
 
 const app =express()
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ origin: "http://localhost:5173", credentials: true }))
@@ -18,6 +20,14 @@ dotenv.config()
 
 
 app.use("/api/auth", authRoutes)
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT,()=>{
     connectDB()
