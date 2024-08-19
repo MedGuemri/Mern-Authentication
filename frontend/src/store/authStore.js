276c46fa38e522824fa6023c1f0e470c022c7f8c
Mohamed Guemri <mohamedamiineguemri@gmail.com>
2024-08-20 01:34:45 +0200

    update meta env mode

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand"
 import axios from "axios"
 
 axios.defaults.withCredentials=true
-const API_URL ="http://localhost:5000/api/auth"
+const API_URL =import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 export const useAuthStore=create((set)=>({
     user :null,
     isAuthenticated:false,
